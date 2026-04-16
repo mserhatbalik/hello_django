f0ba69bc349487671d7e7fc8a1d26ae4aace4948
Serhat <mserhatbalik@gmail.com>
2026-04-16 15:11:25 +0300

    Added comments

--- hello_django/urls.py
+++ hello_django/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("pages.urls"))
+    path("", include("pages.urls")) # Root domain points to pages app urls.
 ]
--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+# Import views.py from current directory.
+# Use .. to refer to parent directory. Use ... to refer to grandparent directory.
+# Using without dot notation refers to sys.path packages, which is not what we want in this situation.
 from .views import home_page_view
 
 urlpatterns = [
